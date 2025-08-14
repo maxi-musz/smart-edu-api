67b34188ad12957da24e94f30dbe7ae07150bb97
Maximus <bernardmayowaa@gmail.com>
2025-08-14 11:52:16 +0100

    add create a class

--- src/school/director/classes/classes.controller.ts
+++ src/school/director/classes/classes.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { JwtGuard } from 'src/school/auth/guard';
 import { GetUser } from 'src/school/auth/decorator';
 import { User } from '@prisma/client';
 import { ApiTags } from '@nestjs/swagger';
-import { GetAllClassesDocs } from 'src/docs/director/classes';
+import { GetAllClassesDocs, CreateClassDocs } from 'src/docs/director/classes';
+import { CreateClassDto } from './dto/class.dto';
 
 @ApiTags('Classes')
 @Controller('director/classes')
@@ -22,5 +23,18 @@ export class ClassesController {
   @GetAllClassesDocs.response401
   async getAllClasses(@GetUser() user: User) {
     return this.classesService.getAllClasses(user);
+  }
+
+  // Create a new class
+  // POST /api/v1/director/classes/create-class
+  // Protected endpoint
+  @Post("create-class")
+  @CreateClassDocs.bearerAuth
+  @CreateClassDocs.operation
+  @CreateClassDocs.response200
+  @CreateClassDocs.response400
+  @CreateClassDocs.response401
+  async createClass(@GetUser() user: User, @Body() createClassDto: CreateClassDto) {
+    return this.classesService.createClass(user, createClassDto);
   }
 }
