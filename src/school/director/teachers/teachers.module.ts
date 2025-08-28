94e54eba3f70f766ef703bd5735fb46dfa5387f2
Maximus <bernardmayowaa@gmail.com>
2025-08-28 15:37:07 +0100

    added academic session

--- src/school/director/teachers/teachers.module.ts
+++ src/school/director/teachers/teachers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { TeachersController } from './teachers.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AcademicSessionModule } from 'src/academic-session/academic-session.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AcademicSessionModule],
     controllers: [TeachersController],
     providers: [TeachersService],
     exports: [TeachersService]
